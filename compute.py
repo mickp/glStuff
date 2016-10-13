94a0586105b382a15edbc59b122a861f38650c02
Mick Phillips <mick.phillips@gmail.com>
2016-10-13 17:26:33 +0100

    Trying QtOpenGL stuff.

---
+++ compute.py
@@ -0,0 +1,80 @@
+import ctypes
+
+import numpy
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+from PyQt4 import QtCore, QtGui, QtOpenGL
+
+
+# from PyQt4 import QtGui, QtOpenGL
+class MyWidget(QtOpenGL.QGLWidget):
+    def initializeGL(self):
+        glViewport(0, 0, self.width(), self.height())
+        glEnable(GL_PROGRAM_POINT_SIZE)
+        # compile shaders and program
+        self.shaderProgram = QtOpenGL.QGLShaderProgram()
+        self.shaderProgram.addShaderFromSourceFile(QtOpenGL.QGLShader.Vertex, "meshVertex.glsl")
+        self.shaderProgram.addShaderFromSourceFile(QtOpenGL.QGLShader.Fragment, "meshFragment.glsl")
+        self.shaderProgram.link()
+        # triangle position and color
+        vertexData = numpy.array([0.0, 0.5, 0.0, 1.0,
+                                  0.5, -0.366, 0.0, 1.0,
+                                  -0.5, -0.366, 0.0, 1.0,
+                                  1.0, 0.0, 0.0, 1.0,
+                                  0.0, 1.0, 0.0, 1.0,
+                                  0.0, 0.0, 1.0, 1.0, ],
+                                 dtype=numpy.float32)
+
+        # create VAO
+        self.VAO = glGenVertexArrays(1)
+        glBindVertexArray(self.VAO)
+
+        # create VBO
+        VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)
+        glBufferData(GL_ARRAY_BUFFER, vertexData.nbytes, vertexData, GL_STATIC_DRAW)
+
+        # enable array and set up data
+        glEnableVertexAttribArray(0)
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, None)
+        # the last parameter is a pointer
+        # python donot have pointer, have to using ctypes
+        glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, ctypes.c_void_p(48))
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0)
+        glBindVertexArray(0)
+
+    def paintGL(self):
+        glClearColor(0, 0, 0, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # active shader program
+        self.shaderProgram.bind()
+
+        glBindVertexArray(self.VAO)
+
+        # draw triangle
+        glDrawArrays(GL_POINTS, 0, 3)
+
+        glBindVertexArray(0)
+        
+        self.shaderProgram.release()
+
+
+def main():
+    import sys
+
+    app = QtGui.QApplication(sys.argv)
+
+    glformat = QtOpenGL.QGLFormat()
+    glformat.setVersion(3, 3)
+    glformat.setProfile(QtOpenGL.QGLFormat.CoreProfile)
+    w = MyWidget(glformat)
+    w.resize(640, 480)
+    w.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
